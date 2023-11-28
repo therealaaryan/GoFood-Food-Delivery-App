1444a6c8dfed5f1f17ce2d138f62b3953d4c9be3
Aaryan Mehta <aaryanmta@gmail.com>
2023-11-28 13:02:19 +0530

    Added Navbar,footer,carousel among other screens/components

---
+++ mernapp/src/components/Carousel.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Carousel() {
+    return (
+        <div>
+            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://source.unsplash.com/random/900×700/?burger" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://source.unsplash.com/random/900×700/?pastry" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://source.unsplash.com/random/900×700/?barbeque" className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
   return (
     <div>
-    <div><Navbar/></div>
-    <div>Body</div>
-    <div><Footer/></div>
+      <div><Navbar /></div>
+      <div><Carousel/></div>
+      <div>
+        <Card />
+      </div>
+      <div><Footer /></div>
     </div>
   )
 }
