a8f617fb42ec7d2d8fe6e04fc4886c0863860b01
Aaryan Mehta <aaryanmta@gmail.com>
2023-12-12 18:15:08 +0530

    created model, schema and routes

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://goFood:OreoUchiha@cluster0.zdp7p1f.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://goFood:OreoUchiha@cluster0.zdp7p1f.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
-const mongoDB =  async () => {
-    await mongoose.connect(mongoURI)
-    console.log("Database has been connected!");
-};
+const mongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected");
+        let fetched_data = mongoose.connection.db.collection("food_items");
+        let data = await fetched_data.find({}).toArray()
+        console.log();
+    } catch (error) {
+        console.log(error);
+    }
 
+}
 
 module.exports = mongoDB;
+
--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -8,6 +8,8 @@ mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(express.json())
+app.use('/api', require('./Routes/CreateUser'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
