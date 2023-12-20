b6af008943ef7950830235b0ee9d407f1eb8c57f
Aaryan Mehta <aaryanmta@gmail.com>
2023-12-20 12:40:56 +0530

    added validator to backend

--- mernapp/backend/Routes/CreateUser.js
+++ mernapp/backend/Routes/CreateUser.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post('/createuser', async (req, res) => {
+router.post('/createuser', [
+    body('email').isEmail(),
+    body('name').isLength({ min: 5 }),
+    body('password', 'Incorrect Password').isLength({ min: 5 })], async (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
     try {
         await User.create({
             name: req.body.name,
