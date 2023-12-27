8c5a9cfdb5313250f5de6cc80699dd5220288e48
Aaryan Mehta <aaryanmta@gmail.com>
2023-12-27 20:20:46 +0530

    login done

--- mernapp/backend/Routes/CreateUser.js
+++ mernapp/backend/Routes/CreateUser.js
@@ -7,26 +7,54 @@ router.post('/createuser', [
     body('email').isEmail(),
     body('name').isLength({ min: 5 }),
     body('password', 'Incorrect Password').isLength({ min: 5 })], async (req, res) => {
-    
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() });
-    }
-    
-    try {
-        await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            location: req.body.location
-        })
 
-    res.json({success:true})    
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    } catch (error) {
-        console.log(error)
-        res.json({success:false})
-    }
-})
+        try {
+            await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email,
+                location: req.body.location
+            })
+
+            res.json({ success: true })
+
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false })
+        }
+    })
+
+router.post('/loginuser', [
+    body('email').isEmail(),
+    body('password', 'Incorrect Password').isLength({ min: 5 })], async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let email = req.body.email;
+
+        try {
+            let userData = await User.findOne({ email });
+            if (!userData) {
+                return res.status(400).json({ errors: "Try logging in with correct credentials" });
+            }
+
+            if (req.body.password !== userData.password) {
+                return res.status(400).json({ errors: "Try logging in with correct credentials" });
+            }
+
+            return res.json({ success: true });
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false })
+        }
+    })
 
 module.exports = router;
