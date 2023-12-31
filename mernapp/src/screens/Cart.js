4ecd6ad378570f858329a1353d6564b25b93862a
Aaryan Mehta <aaryanmta@gmail.com>
2023-12-31 13:38:02 +0530

    last changes

--- mernapp/src/screens/Cart.js
+++ mernapp/src/screens/Cart.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Delete from "@material-ui/icons/Delete"
-// import trash from "../trash.svg"
+
 import { useCart, useDispatchCart } from "../components/ContextReducer";
 
 export default function Cart() {
     let data = useCart();
     let dispatch = useDispatchCart();
+
     if(data.length === 0){
         return (
             <div>
@@ -17,56 +17,64 @@ export default function Cart() {
     const handleCheckOut = async() => {
         let userEmail = localStorage.getItem("userEmail");
         let response = await fetch("https://gofood-v7qb.onrender.com/api/orderData", {
-            method:'POST',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify({
-                order_data:data,
-                email:userEmail,
+            body: JSON.stringify({
+                order_data: data,
+                email: userEmail,
                 order_date: new Date().toDateString()
             })
-        }
-        );
-        
-        if(response.status===200){
-            dispatch({type:"DROP"})
+        });
+
+        if(response.status === 200){
+            dispatch({ type: "DROP" })
         }
     }
+
     let totalPrice = data.reduce((total, food) => total + food.price, 0);
+
     return (
         <div>
-            <div className="container m-auto mt-5 table-responsive table-responsive-sm table-responsive-md">
+            <div className="container m-auto mt-5 table-responsive">
                 <table className="table table-hover">
-
-                <thead className="text-success fs-4">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Option</th>
-                        <th scope="col">Amount</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((food, index) => (
+                    <thead className="text-success fs-4">
                         <tr>
-                            <th scope="row" >{index + 1}</th>
-                            <td>{food.name}</td>
-                            <td>{food.qty}</td>
-                            <td>{food.size}</td>
-                            <td>{food.price}</td>
-                            <td><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index})}}/></button></td>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Option</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Action</th>
                         </tr>
-                    ))}
-                </tbody>
+                    </thead>
+                    <tbody>
+                        {data.map((food, index) => (
+                            <tr key={index}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{food.name}</td>
+                                <td>{food.qty}</td>
+                                <td>{food.size}</td>
+                                <td>{food.price}</td>
+                                <td>
+                                    <button 
+                                        type="button" 
+                                        className="btn p-0 text-danger" 
+                                        onClick={() => dispatch({ type: "REMOVE", index })}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
                 <div><h1 className="fs-2">Total Price: {totalPrice}</h1></div>
-                  <div>
-                   <button className="btn bg-success mt-5" onClick={handleCheckOut}> Check Out </button>
+                <div>
+                    <button className="btn bg-success mt-5" onClick={handleCheckOut}> Check Out </button>
                 </div> 
             </div>
         </div>
-    )
+    );
 }
